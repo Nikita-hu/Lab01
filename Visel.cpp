8beefb5f3fa83dba6492dc03bed12148b379a0b2
ToksarovArtemiy <toksarov228@gmail.com>
2024-12-09 12:31:17 +0500

    Visel.cpp

---
+++ Visel.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "");
+
+    cout << "Игра в 'Виселицу' " << endl;
+    string slovo, otgad = "_";
+    int att, mode;
+
+    cout << " Веберите режим игры! ";
+    cout << " Введите: " << endl;
+    cout << " 1 - компьютер загадывает слово " << endl;
+    cout << " 2 - другой человек отгадывает слово " << endl;
+    cin >> mode;
+    while (mode < 1 or mode > 3) {
+        cout << "Введено неверное число!" << endl;
+        cout << "Введите 1 или 2, в замвисимости от желаемого режима игры: ";
+        cin >> mode;
+    }
+
+    if (mode == 2) IgraIgrok();
+    if (mode == 1) IgraKomp();
+        return 0;
+    }
